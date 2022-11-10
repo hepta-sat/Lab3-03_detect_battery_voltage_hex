b3e5eefa57a925530deb4fcd0f26da407b6f436d
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-10 15:45:32 +0000

    change to rawserial

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "HEPTA_EPS.h"
+ 
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(p16,p26);
+ 
+int main()
+{
+    float bt_dec;
+    char bt_hex[4];
+    for(int i = 0; i < 10; i++) {
+        eps.vol(&bt_dec);
+        eps.vol_u16(bt_hex);
+        pc.printf("Vol = %f\r\n",bt_dec);
+        pc.printf("Vol_hex = %c%c%c%c\r\n",bt_hex[0],bt_hex[1],bt_hex[2],bt_hex[3]);
+        wait(1.0);
+    }
 }
